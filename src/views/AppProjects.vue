106efc2d479edc5bd440fe7a2092270e5e65033f
mattemutti <mattemutti@gmail.com>
2024-08-19 16:03:04 +0200

    update url api

--- src/views/AppProjects.vue
+++ src/views/AppProjects.vue
@@ -12,8 +12,8 @@ export default {
 
 	data() {
 		return {
-			base_api_url: 'https://forge.laravel.com',
-			base_project_url: '/api/v1/servers/829590/databases/1182003/',
+			base_api_url: 'https://muttimatteo.com',
+			base_project_url: '/api/projects',
 			projects: [],
 			loading: true,
 			id: [],
@@ -22,12 +22,7 @@ export default {
 	},
 	methods: {
 		callApi(url) {
-			axios.get(url, {
-				headers: {
-					'Access-Control-Allow-Origin': '*',
-					'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMzM0YzFiYTliNzM4MTc2ZGY4N2ViZTMxYTJhNDllMTg3Y2Y2NWVmOGY5MWVjMjJlN2FjNzcwZGM4ZGM1NTk1MjU0Y2QwNmIyZGFjY2RmYTYiLCJpYXQiOjE3MjMxODc4MzcuNTU1MTE3LCJuYmYiOjE3MjMxODc4MzcuNTU1MTE5LCJleHAiOjIwMzg3MjA2MzcuNTM3NDU5LCJzdWIiOiIyMzE1NTciLCJzY29wZXMiOltdfQ.j50XPLohV4f5JigoYU6yIP1meEbtcBql_39iOLtHmYHP8x_ds5hFNvYsgFl8DS5EJFYyy3BUWY1rxsL_uT7hQibyFXnfQca_zQraxZaC1aV9mzhezWSPddqenm4TOfafbGOlw9 - kIqDbaVMEqdSXfqhtgdiE_ahthBz7WTLXJGr2OrKA0lvRsoHICXY2VzbEacYSH8haEfVZvyzsg9O0LMQAf38tjtF7dSGNLftc4b5kMhlqp0_JOusjGXOP9uH7kbsv7ZLfeUzLKd2YT_xdu79jGWFu4N3AzqfA4B8KhmGiG1S - 8MAioTn2pwSJHXk2ha94hpFgi1rEVw2Nm_dnK7sdWIiWn7urlYDpfoxTwWWcUoHsmRoCIkOfaiGrjlOi9qlZydfKbGe4o8R3C5smEextq5a8_87eOwOp5fcOy5MiXzvtQY1oKFUMhxh3sCgVnThs1fDMMveKOWxJP_DB5teXHuL2MwThBaMHsyGb8tvIBgrOPtnjZAolAHM4HYU6IPF6tCXg0FDG3XfvNcq1w1- yjtX93w8O1_3jAnh_E9Q7AxW03OrilcPJU - MVQ9dJ3Zqs5LEc8ZN5_xUxoW282oWYJ604zppWteWH776NLKW8dQkQN9iQWtnygBMAc_YeSJtkIKNjXEjD_a0y5y0FRBzvh - yxed0RGdL1zQXcdxI',
-				},
-			})
+			axios.get(url)
 				.then(response => {
 					console.log(response);
 					this.projects = response.data.projects;
