a16281158f9d855245dc2fea109a152939c7a1b7
mattemutti <mattemutti@gmail.com>
2024-09-03 10:21:25 +0200

    update video single project

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -56,7 +56,7 @@ export default {
 		</div>
 		<div class="text-center video_section">
 			<section>
-				<iframe {{ project.description }}></iframe>
+				{{ project.description }}
 			</section>
 
 
