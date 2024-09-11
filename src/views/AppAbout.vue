668b1b5968914b764fad06f7d71da26aa31af4ef
mattemutti <mattemutti@gmail.com>
2024-09-11 14:54:32 +0200

    update about page

--- src/views/AppAbout.vue
+++ src/views/AppAbout.vue
@@ -11,36 +11,27 @@ export default {
 	</div>
 	<div class="container">
 		<div>
-			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat dolor praesentium omnis et eaque,
-				itaque
-				impedit iure perspiciatis, aspernatur quam ipsam voluptate facere mollitia atque. Architecto ipsa
-				delectus
-				perspiciatis ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea veritatis nam suscipit ut
-				delectus quo dolore non aspernatur eum est laudantium quos, animi velit harum vel fugit assumenda
-				quibusdam
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste et, perferendis animi eos nobis, delectus
-				veniam excepturi fugit ab consequuntur non, nulla eaque recusandae cumque eum repellat quod minus.
-				Suscipit! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat dolor praesentium omnis et
-				eaque,
-				itaque
-				impedit iure perspiciatis, aspernatur quam ipsam voluptate facere mollitia atque. Architecto ipsa
-				delectus
-				perspiciatis ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea veritatis nam suscipit ut
-				delectus quo dolore non aspernatur eum est laudantium quos, animi velit harum vel fugit assumenda
-				quibusdam
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste et, perferendis animi eos nobis, delectus
-				veniam excepturi fugit ab consequuntur non, nulla eaque recusandae cumque eum repellat quod minus.
-				Suscipit!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat dolor praesentium omnis et
-				eaque,
-				itaque
-				impedit iure perspiciatis, aspernatur quam ipsam voluptate facere mollitia atque. Architecto ipsa
-				delectus
-				perspiciatis ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea veritatis nam suscipit ut
-				delectus quo dolore non aspernatur eum est laudantium quos, animi velit harum vel fugit assumenda
-				quibusdam
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste et, perferendis animi eos nobis, delectus
-				veniam excepturi fugit ab consequuntur non, nulla eaque recusandae cumque eum repellat quod minus.
-				Suscipit!
+			<p>Ciao mi chiamo Matteo, a 19 anni inizio a studiare basso elettrico. Volendo ampliare e approfondire lo
+				studio di questo strumento, inizio il percorso di studi presso l’Accademia Musicale Ottava di Brescia,
+				dove frequento il corso di armonia e di basso conseguendo il diploma di terzo livello.
+				Per un paio d’anni ho seguito il corso di pianoforte.
+				Compongo musica inedita, ho partecipato a varie masterclass per quanto riguarda la composizione e il
+				songwriting.
+				Di pari passo alla composizione ho scoperto il lavoro tecnico e artistico che si svolge in studio di
+				registrazione e me ne sono innamorato.
+				Da qui è iniziato il mio percorso di studi per specializzarmi in produzione, registrazione, missaggio e
+				masterizzazione della musica.
+				Sono molto curioso e mi piace essere indipendente soprattutto per quanto riguarda la creazione dei
+				contenuti musicali e visual. Questo mi ha portato ad approfondire il mondo della fotografia e del video
+				che sono molto legati anche alla musica. Mi sono formato presso lo studio WhiteWall di Brescia.
+				Ho un mio progetto personale di cui ho curato la parte musicale, visual e contenuti per i social.
+				Oltre ai progetti personali, offro servizi per artisti o gruppi che vogliono produrre, registrare,
+				missare, masterizzare le loro idee musicali e servizi di fotografia, videoclip e di creazione di
+				contenuti per quanto riguarda la parte visual.
+				Ultimamente mi sono incuriosito della parte web ed ho iniziato a studiare le tecnologie che si
+				utilizzano per creare siti e web app, iscrivendomi ad un corso professionale.
+				In parallelo suono live il basso, sintetizzatori e tastiere/programmazione, con alcuni progetti cover ed
+				artisti con cui collaboro.
 			</p>
 		</div>
 	</div>
